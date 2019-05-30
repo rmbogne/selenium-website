790f5db7c868c96798b874516dba935019b89f12
rmbogne <50390296+rmbogne@users.noreply.github.com>
2019-05-30 00:23:41 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ sudo docker build ./mysql/ -t db
 sudo docker run -it -d -p 3306:3306 --name mysql db
 
 sudo docker build ./website/ -t web
-sudo docker run -it -d -p 3001:80 --link mysql:db web
+sudo docker run -it -d -p 3002:80 --link mysql:db web
